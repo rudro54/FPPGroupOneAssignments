3752ce36e85709a4231e5ac2280fcf5a5454a626
Abdullah Al Zayed <rudro54@gmail.com>
2024-11-25 17:42:19 -0600

    have done problem one and two in my branch zayed .

--- src/lessonTwo/Prog1.java
+++ src/lessonTwo/Prog1.java
@@ -1,7 +1,52 @@
 package lessonTwo;
 
-public class Prog1 {
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+public  class Prog1 {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        System.out.println("number one");
+
+        Random random = new Random();
+
+        int randomNumber = random.nextInt(1,10);
+
+//        System.out.println(randomNumber);
+        double result =  Math.pow(Math.PI,randomNumber);
+
+
+        System.out.println("the random number was "+randomNumber+" and the result "+result);
+
+
+        System.out.println("number two");
+        System.out.println("using printf");
+        System.out.printf("%.2f",result);
+        System.out.println();
+
+       RandomGenerator randomTwo =  RandomGenerator.getDefault();
+
+       int y = randomTwo.nextInt(3,15);
+
+       double resultTwo = Math.pow(y,randomNumber);
+
+        System.out.println("the random number was "+y+ " and the result "+resultTwo);
+        System.out.println("using printf");
+        System.out.printf("%.2f",resultTwo);
+        System.out.println();
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
--- src/lessonTwo/Prog2.java
+++ src/lessonTwo/Prog2.java
@@ -1,11 +1,26 @@
 package lessonTwo;
 
+import java.util.Random;
+
 public class Prog2 {
 
-    
-
-
     public static void main(String[] args) {
+
+      float a=1.27f;
+      float b=3.881f;
+      float c=9.6f;
+
+      float summation = a+b+c;
+
+      int sum = (int)summation;
+
+        System.out.println("before changing "+summation);
+        System.out.println("after type casting the sum is "+sum);
+
+        System.out.println("rounding up using math dot round "+Math.round(summation));
+
+
+
 
     }
 }
