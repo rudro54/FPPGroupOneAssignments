58c437b0ccc006a3c84c653f31bb86355fa3791f
Sudipto Roy <sroy@miu.edu>
2024-12-01 22:17:20 -0600

    solution of problem 4

--- src/lessonThree/Main.java
+++ src/lessonThree/Main.java
@@ -84,7 +84,35 @@ public class Main {
         event.convertToTimeZone(targetTimeZone);
 
         //Solution of Problem4
-        //In progress
+        System.out.println("Enter C for Circle");
+        System.out.println("Enter R for Rectangle");
+        System.out.println("Enter T for Triangle");
+        String choice = sc.nextLine().toUpperCase();
+        switch (choice) {
+            case "C" -> {
+                System.out.println("Enter the radius of the Circle:");
+                double radius = sc.nextDouble();
+                Circle circle = new Circle(radius);
+                System.out.printf("The area of Circle is: %.2f%n", circle.computeArea());
+            }
+            case "R" -> {
+                System.out.println("Enter the width of the Rectangle:");
+                double width = sc.nextDouble();
+                System.out.println("Enter the height of the Rectangle:");
+                double height = sc.nextDouble();
+                Rectangle rectangle = new Rectangle(width, height);
+                System.out.printf("The area of Rectangle is: %.2f%n", rectangle.computeArea());
+            }
+            case "T" -> {
+                System.out.println("Enter the base of the Triangle:");
+                double base = sc.nextDouble();
+                System.out.println("Enter the height of the Triangle:");
+                double height = sc.nextDouble();
+                Triangle triangle = new Triangle(base, height);
+                System.out.printf("The area of Triangle is: %.2f%n", triangle.computeArea());
+            }
+            default -> System.out.println("Unsupported format");
+        }
     }
 
 
