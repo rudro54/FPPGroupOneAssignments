f0d2926b49146248137c7775176edd9f9544c47c
Sudipto Roy <sroy@miu.edu>
2024-12-13 14:55:40 -0600

    lesson 11 structure

---
+++ src/lesson11/problem1/src/Admin.java
@@ -0,0 +1,8 @@
+package lesson11.problem1.src;
+import java.util.*;
+public class Admin {
+	public static HashMap<Key, Student> processStudents(List<Student> students) {
+		//implement
+		return null;
+	}
+}
---
+++ src/lesson11/problem1/src/Standing.java
@@ -0,0 +1,5 @@
+package lesson11.problem1.src;
+
+public enum Standing {
+	FRESHMAN, SOPHOMORE, JUNIOR, SENIOR;
+}
